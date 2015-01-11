28a6ae69b7a502d81511058c175998f2e8424f25
nuthanmunaiah <nuthan.munaiah@live.com>
2015-01-11 18:24:19 -0500

    Removed constants used to denote reachability type. The model does not capture reachability anymore. Deferring that to analysis phase.

--- app/constants.py
+++ app/constants.py
@@ -1,15 +1,5 @@
 import re
 
-RT_EPR = 'epr'
-RT_EXPR = 'expr'
-RT_SEPR = 'sepr'
-
-REACHABILITY_TYPE = (
-    (RT_EPR, 'Entry Point Reachability'),
-    (RT_EXPR, 'Exit Point Reachability'),
-    (RT_SEPR, 'Shallow Entry Point Reachability'),
-)
-
 RT_BRANCH = 'b'
 RT_TAG = 't'
 
@@ -21,5 +11,4 @@ REVISION_TYPE = (
 CALLGRAPH_FILE_PATTERN = '%s_%s_%s.txt'
 
 RE_REV_NUM = re.compile('(\d+)(?:\.(\d+))?(?:\.(\d+))?')
-# TODO: Review return types used here
-RE_FUNC_AFFECTED = re.compile('^(@@.*@@ .*(void|int)\s)(\w*)(\(.*)$')
+RE_FUNC_AFFECTED = re.compile('(\w*)(?:\s?\()')
