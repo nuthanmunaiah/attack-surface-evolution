2b80e30b74e829a56a429ed30bbd855cbb0d0711
nuthanmunaiah <nuthan.munaiah@live.com>
2015-01-31 14:35:37 -0500

    Removed *_dist_to_entry, *_dist_to_exit fields, num_entry_points, and num_exit_points. Added fields to hold metrics proximity, surface coupling, and attack surface betweenness.

--- app/models.py
+++ app/models.py
@@ -13,8 +13,8 @@ class Revision(models.Model):
 
     num_entry_points = models.PositiveIntegerField(default=0, blank=False)
     num_exit_points = models.PositiveIntegerField(default=0, blank=False)
-    num_functions =  models.PositiveIntegerField(default=0, blank=False)
-    num_reachable_functions = models.PositiveIntegerField(default=0, blank=False)
+    num_functions = models.PositiveIntegerField(default=0, blank=False)
+    num_attack_surface_functions = models.PositiveIntegerField(default=0, blank=False)
 
     class Meta:
         unique_together = ('number', 'type')
@@ -27,16 +27,12 @@ class Function(models.Model):
     is_entry = models.BooleanField(default=False)
     is_exit = models.BooleanField(default=False)
     is_vulnerable = models.BooleanField(default=False)
-    min_dist_to_entry = models.PositiveIntegerField(default=None, null=True)
-    max_dist_to_entry = models.PositiveIntegerField(default=None, null=True)
-    avg_dist_to_entry = models.PositiveIntegerField(default=None, null=True)
-    num_entry_points = models.PositiveIntegerField(default=None, null=True)
-    min_dist_to_exit = models.PositiveIntegerField(default=None, null=True)
-    max_dist_to_exit = models.PositiveIntegerField(default=None, null=True)
-    avg_dist_to_exit = models.PositiveIntegerField(default=None, null=True)
-    num_exit_points = models.PositiveIntegerField(default=None, null=True)
-    attack_surface_betweenness = models.FloatField(default=None, null=True)
     sloc = models.PositiveIntegerField(default=None, null=True)
+    proximity_to_entry = models.FloatField(default=None, null=True)
+    proximity_to_exit = models.FloatField(default=None, null=True)
+    surface_coupling_with_entry = models.PositiveIntegerField(default=None, null=True)
+    surface_coupling_with_exit = models.PositiveIntegerField(default=None, null=True)
+    attack_surface_betweenness = models.FloatField(default=None, null=True)
 
 
 class Reachability(models.Model):
