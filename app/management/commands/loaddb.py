95626c44b0973cad4548aaa0d65c26db3189e185
nuthanmunaiah <nuthan.munaiah@live.com>
2015-02-02 13:15:03 -0500

    Introduced threading with the hope that it would improve performance. No perceivable improvement in performance observed. Committing the change anyway to try alternatives such as multi-processing and distributed processing.

--- app/management/commands/loaddb.py
+++ app/management/commands/loaddb.py
@@ -1,4 +1,4 @@
-import os, statistics, subprocess, datetime, csv, sys
+import os, statistics, subprocess, datetime, csv, sys, threading, multiprocessing
 from optparse import make_option
 from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
@@ -9,7 +9,7 @@ from attacksurfacemeter.call_graph import CallGraph
 from loaders.cflow_loader import CflowLoader
 from loaders.gprof_loader import GprofLoader
 
-from app.models import *
+from app import models
 from app import constants
 from app import gitapi
 
@@ -30,7 +30,7 @@ class Command(BaseCommand):
                     help='rev-num HELP.'),
         make_option('-t', '--rev-type',
                     dest='rev_type',
-                    default='t',
+                    default='b',
                     help='rev-type HELP.'),
     )
     help = 'loaddb HELP.'
@@ -40,15 +40,18 @@ class Command(BaseCommand):
 
         revisions = None
         if self.rev_num:
-            revisions = Revision.objects.filter(number=self.rev_num, type=self.rev_type)
+            revisions = models.Revision.objects.filter(number=self.rev_num, type=self.rev_type)
         else:
-            revisions = Revision.objects.filter(is_loaded=False)
+            revisions = models.Revision.objects.filter(is_loaded=False)
 
         if revisions.count() == 0:
             raise CommandError('No revisions to mine. Aborting.')
         else:
+            utilities = Utilities(self.repository_path, self.workspace_path)
             for revision in revisions:
-                self.mine(revision)
+                rev = Revision(revision, utilities)
+                rev.init()
+                rev.mine()
 
     def validate_arguments(self, **options):
         self.verbosity = int(options.get('verbosity'))
@@ -66,119 +69,136 @@ class Command(BaseCommand):
         if self.rev_type not in ['t', 'b']:
             raise CommandError('%s is not a valid revision type.' % self.rev_type)
 
-    def mine(self, revision):
-        print('Mining', revision.number)
+    def write(self, message, verbosity=1):
+        if verbosity >= self.verbosity:
+            self.stdout.write(str(message))
+
+
+class Revision(object):
+    def __init__(self, revision, utilities):
+        if not isinstance(revision, models.Revision):
+            raise CommandError('Argument revision should be of type app.models.Revision.')
+
+        self.revision = revision
+        self.utilities = utilities
+        self.semaphore = threading.BoundedSemaphore(value=25)
+        self.dblock = threading.Lock()
+
+    def init(self):
+        self.vuln_funcs = self.utilities.get_vulnerable_functions(self.revision)
+        self.func_sloc = self.utilities.get_function_sloc(self.revision)
+        self.call_graph = self.utilities.get_call_graph(self.revision)
+        self.attack_surface_betweenness = nx.betweenness_centrality(self.call_graph.attack_surface_graph,
+                                                                    endpoints=True)
+
+    def mine(self):
+        print('Mining', self.revision.number)
 
         # TODO : REMOVE
         start = datetime.datetime.now()
 
-        vuln_funcs = self.get_vulnerable_functions(revision)
-        func_sloc = self.get_function_sloc(revision)
-
-        call_graph = self.get_call_graph(revision)
-
-        attack_surface_betweenness = nx.betweenness_centrality(call_graph.attack_surface_graph, endpoints=True)
-
-        # TODO : REMOVE
         index = 1
-        total = len(call_graph.nodes)
+        total = len(self.call_graph.nodes)
         with transaction.atomic():
-            for node in call_graph.nodes:
-                # TODO : REMOVE
+            for node in self.call_graph.nodes:
+                self.semaphore.acquire()
+                thread = threading.Thread(target=self.__process_node__, args=(node,))
+                thread.start()
+
                 percent = index / total
-                self.indicate_progress(percent)
                 index += 1
+                self.utilities.indicate_progress(percent)
 
-                function = Function()
-
-                function.revision = revision
-                function.name = node.function_name
-                function.file = node.function_signature
-                function.is_entry = node in call_graph.entry_points
-                function.is_exit = node in call_graph.exit_points
-                function.is_vulnerable = \
-                    (
-                        node.function_signature in vuln_funcs and
-                        node.function_name in vuln_funcs[node.function_signature]
-                    )
-
-                # Fully qualified name of the node in the form function_name@file_name
-                fq_name = '%s@%s' % (node.function_name, node.function_signature)
-                if fq_name in func_sloc:
-                    function.sloc = func_sloc[fq_name]
-
-                if node in call_graph.attack_surface_graph_nodes:
-                    metrics = call_graph.get_entry_surface_metrics(node)
-                    function.proximity_to_entry = metrics['proximity']
-                    function.surface_coupling_with_entry = metrics['surface_coupling']
-
-                    metrics = call_graph.get_exit_surface_metrics(node)
-                    function.proximity_to_exit = metrics['proximity']
-                    function.surface_coupling_with_exit = metrics['surface_coupling']
-
-                if node in attack_surface_betweenness:
-                    function.attack_surface_betweenness = attack_surface_betweenness[node]
-
-                function.save()
-
-                if function.is_entry:
-                    enpr = Reachability()
-                    enpr.type = constants.RT_EN
-                    enpr.function = function
-                    enpr.value = call_graph.get_entry_point_reachability(node)
-                    enpr.save()
-
-                    senpr_within_depth_one = Reachability()
-                    senpr_within_depth_one.type = constants.RT_SHEN_ONE
-                    senpr_within_depth_one.function = function
-                    senpr_within_depth_one.value = call_graph.get_entry_point_reachability(node)
-                    senpr_within_depth_one.save()
-
-                    senpr_within_depth_two = Reachability()
-                    senpr_within_depth_two.type = constants.RT_SHEN_TWO
-                    senpr_within_depth_two.function = function
-                    senpr_within_depth_two.value = call_graph.get_entry_point_reachability(node)
-                    senpr_within_depth_two.save()
-
-                if function.is_exit:
-                    expr = Reachability()
-                    expr.type = constants.RT_EX
-                    expr.function = function
-                    expr.value = call_graph.get_exit_point_reachability(node)
-                    expr.save()
-
-            revision.num_entry_points = len(call_graph.entry_points)
-            revision.num_exit_points = len(call_graph.exit_points)
-            revision.num_functions = len(call_graph.nodes)
-            revision.num_attack_surface_functions = len(call_graph.attack_surface_graph_nodes)
-            revision.is_loaded = True
-            revision.save()
+            self.revision.num_entry_points = len(self.call_graph.entry_points)
+            self.revision.num_exit_points = len(self.call_graph.exit_points)
+            self.revision.num_functions = len(self.call_graph.nodes)
+            self.revision.num_attack_surface_functions = len(self.call_graph.attack_surface_graph_nodes)
+            self.revision.is_loaded = True
+            self.revision.save()
 
         # TODO : REMOVE
         print()
         print('Start\t', start)
         print('Stop\t', datetime.datetime.now())
 
-    def get_call_graph(self, revision):
-        cflow_file = os.path.join(self.workspace_path,
-                                  constants.CALLGRAPH_FILE_PATTERN % (revision.type, revision.number, 'cflow'))
-        gprof_file = os.path.join(self.workspace_path,
-                                  constants.CALLGRAPH_FILE_PATTERN % (revision.type, revision.number, 'gprof'))
+    def __process_node__(self, node):
+        try:
+            function = models.Function()
+            function.revision = self.revision
+            function.name = node.function_name
+            function.file = node.function_signature
+            function.is_entry = node in self.call_graph.entry_points
+            function.is_exit = node in self.call_graph.exit_points
+            function.is_vulnerable = \
+                (
+                    node.function_signature in self.vuln_funcs and
+                    node.function_name in self.vuln_funcs[node.function_signature]
+                )
 
-        if not os.path.exists(cflow_file) or not os.path.exists(gprof_file):
-            raise CommandError('Call graphs file(s) not found at %s.' % self.workspace_path)
+            # Fully qualified name of the node in the form function_name@file_name
+            func_name = '%s@%s' % (node.function_name, node.function_signature)
+            if func_name in self.func_sloc:
+                function.sloc = self.func_sloc[func_name]
 
-        cfl = CflowLoader(cflow_file, True)
-        gpl = GprofLoader(gprof_file, False)
+            if node in self.call_graph.attack_surface_graph_nodes:
+                metrics = self.call_graph.get_entry_surface_metrics(node)
+                function.proximity_to_entry = metrics['proximity']
+                function.surface_coupling_with_entry = metrics['surface_coupling']
 
-        return CallGraph.from_merge(CallGraph.from_loader(cfl), CallGraph.from_loader(gpl))
+                metrics = self.call_graph.get_exit_surface_metrics(node)
+                function.proximity_to_exit = metrics['proximity']
+                function.surface_coupling_with_exit = metrics['surface_coupling']
+
+            if node in self.attack_surface_betweenness:
+                function.attack_surface_betweenness = self.attack_surface_betweenness[node]
+
+            with self.dblock:
+                function.save()
+
+            if function.is_entry:
+                enpr = models.Reachability()
+                enpr.type = constants.RT_EN
+                enpr.function = function
+                enpr.value = self.call_graph.get_entry_point_reachability(node)
+
+                senpr_within_depth_one = models.Reachability()
+                senpr_within_depth_one.type = constants.RT_SHEN_ONE
+                senpr_within_depth_one.function = function
+                senpr_within_depth_one.value = self.call_graph.get_entry_point_reachability(node)
+
+                senpr_within_depth_two = models.Reachability()
+                senpr_within_depth_two.type = constants.RT_SHEN_TWO
+                senpr_within_depth_two.function = function
+                senpr_within_depth_two.value = self.call_graph.get_entry_point_reachability(node)
+
+                with self.dblock:
+                    enpr.save()
+                    senpr_within_depth_one.save()
+                    senpr_within_depth_two.save()
+
+            if function.is_exit:
+                expr = models.Reachability()
+                expr.type = constants.RT_EX
+                expr.function = function
+                expr.value = self.call_graph.get_exit_point_reachability(node)
+
+                with self.dblock:
+                    expr.save()
+        finally:
+            self.semaphore.release()
+
+
+class Utilities(object):
+    def __init__(self, repository_path, workspace_path):
+        self.repository_path = repository_path
+        self.workspace_path = workspace_path
 
     def get_vulnerable_functions(self, revision):
         vuln_fixes = None
         if revision.type == constants.RT_TAG:
-            vuln_fixes = CveRevision.objects.filter(revision=revision)
+            vuln_fixes = models.CveRevision.objects.filter(revision=revision)
         elif revision.type == constants.RT_BRANCH:
-            vuln_fixes = CveRevision.objects.filter(
+            vuln_fixes = models.CveRevision.objects.filter(
                 revision__number__startswith=revision.number[:revision.number.rfind('.')])
 
         repo = gitapi.Repo(self.repository_path)
@@ -199,6 +219,10 @@ class Command(BaseCommand):
 
     def get_function_sloc(self, revision):
         sloc_file = os.path.join(self.workspace_path, constants.FUNC_SLOC_FILE_PATTERN % revision.number)
+
+        if not os.path.exists(sloc_file):
+            raise CommandError('%s not found.' % sloc_file)
+
         function_sloc = dict()
         with open(sloc_file, 'r') as _sloc_file:
             reader = csv.reader(_sloc_file)
@@ -208,9 +232,19 @@ class Command(BaseCommand):
 
         return function_sloc
 
-    def write(self, message, verbosity=1):
-        if verbosity >= self.verbosity:
-            self.stdout.write(str(message))
+    def get_call_graph(self, revision):
+        cflow_file = os.path.join(self.workspace_path,
+                                  constants.CALLGRAPH_FILE_PATTERN % (revision.type, revision.number, 'cflow'))
+        gprof_file = os.path.join(self.workspace_path,
+                                  constants.CALLGRAPH_FILE_PATTERN % (revision.type, revision.number, 'gprof'))
+
+        if not os.path.exists(cflow_file) or not os.path.exists(gprof_file):
+            raise CommandError('Call graphs file(s) not found at %s.' % self.workspace_path)
+
+        cfl = CflowLoader(cflow_file, True)
+        gpl = GprofLoader(gprof_file, False)
+
+        return CallGraph.from_merge(CallGraph.from_loader(cfl), CallGraph.from_loader(gpl))
 
     def indicate_progress(self, percent, bar_length=50):
         sys.stdout.write("\r")
