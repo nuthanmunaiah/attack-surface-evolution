02b09128ee954c853ddcfbb9b98ac9974b67f6b4
nuthanmunaiah <nuthan.munaiah@live.com>
2015-02-03 22:55:40 -0500

    Decorated code that inserted revisions and cves into the database and mapped cve fixes to revisions with transaction.atomic() to prevent partially committed database state.

--- app/management/commands/initdb.py
+++ app/management/commands/initdb.py
@@ -36,29 +36,31 @@ class Command(BaseCommand):
     def load_revisions(self):
         revisions_file = get_absolute_path('assets/data/revisions.txt')
 
-        with open(revisions_file, 'r') as _revisions_file:
-            reader = csv.reader(_revisions_file)
-            for row in reader:
-                if not Revision.objects.filter(number=row[0]).exists():
-                    revision = Revision()
-                    revision.number = get_version_number(row[1])
-                    revision.type = row[0]
-                    revision.ref = row[1]
-                    revision.date = datetime.datetime.strptime(row[2], '%Y-%m-%d').date()
-                    revision.save()
+        with transaction.atomic():
+            with open(revisions_file, 'r') as _revisions_file:
+                reader = csv.reader(_revisions_file)
+                for row in reader:
+                    if not Revision.objects.filter(number=row[0]).exists():
+                        revision = Revision()
+                        revision.number = get_version_number(row[1])
+                        revision.type = row[0]
+                        revision.ref = row[1]
+                        revision.date = datetime.datetime.strptime(row[2], '%Y-%m-%d').date()
+                        revision.save()
 
     def load_cves(self):
         cve_files = [get_absolute_path('assets/data/cves_reported.txt'),
                      get_absolute_path('assets/data/cves_non_ffmpeg.txt')]
 
-        for cve_file in cve_files:
-            with open(cve_file, 'r') as _cve_file:
-                reader = csv.reader(_cve_file)
-                for row in reader:
-                    cve = Cve()
-                    cve.cve_id = row[0]
-                    cve.publish_dt = datetime.datetime.strptime(row[1], '%m/%d/%Y')
-                    cve.save()
+        with transaction.atomic():
+            for cve_file in cve_files:
+                with open(cve_file, 'r') as _cve_file:
+                    reader = csv.reader(_cve_file)
+                    for row in reader:
+                        cve = Cve()
+                        cve.cve_id = row[0]
+                        cve.publish_dt = datetime.datetime.strptime(row[1], '%m/%d/%Y')
+                        cve.save()
 
     def map_cves_to_revisions(self):
         cves_fixed_file = get_absolute_path('assets/data/cves_fixed.txt')
@@ -72,20 +74,20 @@ class Command(BaseCommand):
                 else:
                     fixed_cves[row[1]] = [{'revision': row[0], 'commit_hash': row[2]}]
 
-        for cve in Cve.objects.all():
-            if cve.cve_id in fixed_cves:
-                with transaction.atomic():
-                    cve.is_fixed = True
-                    cve.save()
+        with transaction.atomic():
+            for cve in Cve.objects.all():
+                if cve.cve_id in fixed_cves:
+                    with transaction.atomic():
+                        cve.is_fixed = True
+                        cve.save()
 
-                    for cve_fix in fixed_cves[cve.cve_id]:
-                        rev_num = get_version_number(cve_fix['revision'])
-
-                        cve_revision = CveRevision()
-                        cve_revision.cve = cve
-                        cve_revision.revision = Revision.objects.get(number=rev_num, type=constants.RT_TAG)
-                        cve_revision.commit_hash = cve_fix['commit_hash']
-                        cve_revision.save()
+                        for cve_fix in fixed_cves[cve.cve_id]:
+                            rev_num = get_version_number(cve_fix['revision'])
+                            cve_revision = CveRevision()
+                            cve_revision.cve = cve
+                            cve_revision.revision = Revision.objects.get(number=rev_num, type=constants.RT_TAG)
+                            cve_revision.commit_hash = cve_fix['commit_hash']
+                            cve_revision.save()
 
     def write(self, message, verbosity=1):
         if verbosity >= self.verbosity:
