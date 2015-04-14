0faddb1332534a13e6ebf174640ba1bfe90f68ed
Nuthan Munaiah <nuthan.munaiah@live.com>
2015-04-13 23:07:44 -0400

    Increased memory, corrected ssh tunnel destination port, and added command to kill the ssh tunnel upon completion

--- sbatch.sh
+++ sbatch.sh
@@ -23,7 +23,7 @@
 #SBATCH -p work -n 1 -c 14
 
 # Job memory requirements in MB
-#SBATCH --mem=1
+#SBATCH --mem=14336
 
 # Explicitly state you are a free user
 #SBATCH --qos=free
@@ -33,11 +33,17 @@
 #
 
 module load cflow/1.4
-ssh -i ~/.ssh/id_archeology -f nm6061@archeology.gccis.rit.edu -L 5432:localhost:5342 -N
+
 source venv/bin/activate
 
 # Hack to workaround the race-condition issue when multiple processes attempt
 # to clone to a location that does not exist
 mkdir /tmp/FFmpeg
 
+# SSH tunnel to our database server
+ssh -i ~/.ssh/id_archeology -f nm6061@archeology.gccis.rit.edu -L 5432:localhost:5432 -N
+
 python3 manage.py loaddb
+
+# Closing the SSH tunnel
+kill $(ps -U nm6061 -f | grep 'ssh -i' | head -n 1 | awk '{ print $2 }')
