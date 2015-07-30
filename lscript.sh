f70e37a04ef3100dfe11267c7e575fb5f9fe8d4c
Nuthan Munaiah <nuthan.munaiah@live.com>
2015-07-29 21:39:28 -0400

    SLURM scripts can now work with multiple subjects

--- lscript.sh
+++ lscript.sh
@@ -1,49 +1,28 @@
 #!/bin/bash -l
-# NOTE the -l flag!
-#
 
-# Name of the job - You'll probably want to customize this.
-#SBATCH -J ase-lscript
-
-# Standard out and Standard Error output files
-#SBATCH -o slurm/lscript.slurm.%a.out
-#SBATCH -e slurm/lscript.slurm.%a.err
-
-#To send emails, set the adcdress below and remove one of the "#" signs.
+#SBATCH -p work -n 1 -c 25
 #SBATCH --mail-user nm6061@rit.edu
-
-# notify on state change: BEGIN, END, FAIL or ALL
 #SBATCH --mail-type=ALL
-
-# Maximum run time h:m:s
-#SBATCH -t 3:0:0
-
-# Put the job in the "work" partition and request FOUR cores for one task
-# "work" is the default partition so it can be omitted without issue.
-#SBATCH -p work -n 1 -c 10
-
-# Job memory requirements in MB
-#SBATCH --mem=2048
-
-# Explicitly state you are a free user
 #SBATCH --qos=free
 
-#
-# Your job script goes below this line.
-#
+subject=$1
 
-declare -a releases=("0.5.0" "0.6.0" "0.7.0" "0.8.0" "0.9.0" "0.10.0" "0.11.0" "1.0.0" "1.1.0" "1.2.0" "2.0.0" "2.1.0" "2.2.0" "2.3.0" "2.4.0" "2.5.0")
+if [ $subject == "ffmpeg"  ]; then
+    declare -a releases=(
+        "0.5.0" "0.6.0" "0.7.0" "0.8.0" "0.9.0" "0.10.0" "0.11.0" "1.0.0"
+        "1.1.0" "1.2.0" "2.0.0" "2.1.0" "2.2.0" "2.3.0" "2.4.0" "2.5.0"
+    )
 
-if [ "$#" -eq 1 ]; then
-    SLURM_ARRAY_TASK_ID=$1
-    echo "Loading release ${releases[${SLURM_ARRAY_TASK_ID}]}"
+    export LD_LIBRARY_PATH="$HOME/lib:$HOME/lib64"
+    export PKG_CONFIG_PATH="$HOME/lib/pkgconfig:$HOME/lib64/pkgconfig"
+elif [ $subject == "wireshark" ]; then
+    declare -a releases=(
+        "1.0.0" "1.2.0" "1.4.0" "1.6.0" "1.8.0" "1.10.0" "1.12.0"
+    )
 fi
-
-export LD_LIBRARY_PATH="$HOME/lib:$HOME/lib64"
-export PKG_CONFIG_PATH="$HOME/lib/pkgconfig:$HOME/lib64/pkgconfig"
 
 module load python/3.4.3
 module load cflow/1.4
 source venv/bin/activate
 
-python3 manage.py loaddb -r ${releases[${SLURM_ARRAY_TASK_ID}]}
+DEBUG=1 python3 manage.py loaddb -s $subject -r ${releases[${SLURM_ARRAY_TASK_ID}]}
---
+++ submit.lscript.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+    cat <<EOF
+USAGE: $0 subject indices [partition]
+  subject : The target subject of mining. One of curl, ffmpeg, and wireshark.
+  indices : Index of the release to mine. Check lscript.sh for indices.
+  partition : SLURM partition. One of debug and work. work is the default.
+EOF
+
+    exit 1
+fi
+
+subject=${1,,}
+releases=$2
+partition="work"
+if [ $# -eq 3 ]; then
+    case ${3,,} in
+        "debug")
+            partition="debug"
+            ;;
+        "work")
+            partition="work"
+            ;;
+        *)
+            echo "WARNING: Invalid partition - ${3,,}. Using default (work)."
+            ;;
+    esac
+fi
+
+case $subject in
+    "curl")
+        echo "-----------------------------------------"
+        echo "Subject: cURL"
+        echo "-----------------------------------------"
+        
+        echo "No support for $subject"
+        exit 1
+        ;;
+    "ffmpeg")
+        echo "-----------------------------------------"
+        echo "Subject: FFmpeg"
+        echo "-----------------------------------------"
+    
+        # SLURM: FFmpeg-specific arguments
+        memory=12288    # 12 GiB
+        duration=10:0:0
+        ;;
+    "wireshark")
+        echo "-----------------------------------------"
+        echo "Subject: Wireshark"
+        echo "-----------------------------------------"
+    
+        # SLURM: Wireshark-specific arguments
+        memory=51200    # 50 GiB
+        duration=48:0:0
+        ;;
+    *)
+        echo "ERROR: Invalid subject - $subject."
+        exit 1
+        ;;
+esac
+
+if [ ! -d "slurm" ]; then
+    mkdir slurm
+fi
+if [ ! -d "slurm/$subject" ]; then
+    mkdir slurm/$subject
+fi
+stdout="slurm/${subject}/%a.lscript.out"
+stderr="slurm/${subject}/%a.lscript.err"
+
+sbatch --job-name="ASEL-$subject" \
+    --output=$stdout \
+    --error=$stderr \
+    --time=$duration \
+    --mem=$memory \
+    --partition=$partition \
+    --array=$releases \
+    lscript.sh $subject > /dev/null
+
+if [ $? -eq 0 ]; then
+    echo "INFO: Submitted SLURM job to load $subject."
+    echo "INFO: Submission parameters were:"
+    echo "  Duration $duration"
+    echo "  Memory   $memory"
+    echo "  Releases $releases"
+    echo "  Output   $stdout"
+    echo "  Error    $stderr"
+else
+    echo "ERROR: Submitting SLURM job to load $subject failed."
+fi
+echo "-----------------------------------------"
