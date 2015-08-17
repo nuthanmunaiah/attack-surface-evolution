6ff0abec12e0880230c40458309ffe3619707234
Nuthan Munaiah <nuthan.munaiah@live.com>
2015-08-17 17:45:02 -0400

    Added gprof analysis code to Wireshark

--- app/subjects/wireshark.py
+++ app/subjects/wireshark.py
@@ -27,13 +27,45 @@ class Wireshark(subject.Subject):
         )
 
     def configure(self):
-        return 0
+        self.__dbug__('Configuring {0}'.format(self.name))
+
+        cmd = './autogen.sh'
+        returncode = self.execute(cmd)
+        if returncode == 0:
+            cmd = './configure {0}'.format(self.configure_options)
+            returncode = self.execute(cmd)
+        return returncode
 
     def make(self):
-        return 0
+        self.__dbug__('Building {0}'.format(self.name))
+        cmd = 'make -j %d' % self.processes
+        return self.execute(cmd)
 
     def test(self):
-        return 0
+        # Returning non-zero return value to allow execution of manual script
+        return 2
 
     def __gprof__(self, gmon_file_path, gprof_file_path):
-        return 0
+        self.__dbug__(
+            'Generating call graph for {0} using gprof with profile '
+            'information from {1}'.format(self.name, gmon_file_path)
+        )
+        cmd = 'gprof -q -b -l -c -L .libs/wireshark {0}'
+        cmd = cmd.format(gmon_file_path)
+
+        with open(gprof_file_path, 'w+') as _gprof_file:
+            returncode = self.execute(cmd, stdout=_gprof_file)
+
+        # gprof's -L is printing absolute path instead of relative path.
+        #   Fixing the paths using sed.
+        # Examples:
+        #   /home/rady/wireshark/wireshark.c     > ./wireshark.c
+
+        self.execute(
+            "sed -i 's;{0};.;g' {1}".format(
+                self.source_dir.replace('/', '\/'),
+                gprof_file_path
+            )
+        )
+
+        return returncode
