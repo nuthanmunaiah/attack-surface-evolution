1cc8a277773d02574e1d5d81d300e7bc693e15e2
Nuthan Munaiah <nuthan.munaiah@live.com>
2015-12-06 19:44:19 -0500

    Added factory method SubjectCreator.from_subject
    Factory method accepts an instance of type app.models.Subject and
    returns a corresponding instance of a class that inherits from
    app.subjects.Subject. If a matching class does not exits,
    SubjectUnknownError is raised.

--- app/errors.py
+++ app/errors.py
@@ -12,3 +12,11 @@ class SubjectNotPreparedError(Exception):
 
     def __str__(self):
         return repr(self.value)
+
+
+class UnknownSubjectError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
--- app/subjects/__init__.py
+++ app/subjects/__init__.py
@@ -1,1 +1,20 @@
+from app.errors import UnknownSubjectError
+from app.subjects import curl, ffmpeg, wireshark
 
+
+class SubjectCreator(object):
+    @staticmethod
+    def from_subject(subject, scratch_root='~'):
+        cls = SubjectCreator._get_subject_cls(subject)
+        return cls(subject.name, subject.remote, scratch_root)
+
+    @staticmethod
+    def _get_subject_cls(subject):
+        if subject.name.lower() == 'ffmpeg':
+            return ffmpeg.FFmpeg
+        if subject.name.lower() == 'wireshark':
+            return wireshark.Wireshark
+        if subject.name.lower() == 'curl':
+            return curl.cURL
+
+        raise UnknownSubjectError('Unknown Subject: {0}'.format(subject.name))
