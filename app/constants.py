fdbb63f0f2d5957f0ce498a14894c1204da1a37f
Nuthan Munaiah <nuthan.munaiah@live.com>
2015-12-06 19:52:08 -0500

    Added SUBJECTS and ASSETS_ROOT_URL
    SUBJECTS - A dictionary with the name of the subject as key and the
    URL to GitHub repository as the value.
    ASSETS_ROOT_URL - URL to the Google Drive root directory containing
    addition assests such as list of function SLOC, list of designed
    defenses, and patches (if any).

--- app/constants.py
+++ app/constants.py
@@ -1,5 +1,6 @@
 import re
 
+
 RT_EN = 'en'
 RT_EX = 'ex'
 
@@ -20,3 +21,13 @@ CALLGRAPH_FILE_PATTERN = '%s_%s_%s.txt'
 FUNC_SLOC_FILE_PATTERN = '%s.ffmpeg.csv'
 
 RE_REV_NUM = re.compile('(\d+)(?:[\._](\d+))?(?:[\.\_](\d+))?')
+
+SUBJECTS = {
+        'ffmpeg': 'https://github.com/ffmpeg/ffmpeg',
+        'wireshark': 'https://github.com/wireshark/wireshark',
+    }
+
+ASSETS_ROOT_URL = (
+    'https://5751f28e22fbde2d8ba3f9b75936e0767c761c6a.googledrive.com/host/0B1'
+    'eWsh8KZjRrfjg0Z1VkcU96U2hQal9scHM3NzVmYTk2WVhiQzQtdGFpNWc5c0VzbUJFTE0'
+)
