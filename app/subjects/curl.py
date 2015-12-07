ba61d4166fe7bce9f39f24feb0a3f32458555eb4
Nuthan Munaiah <nuthan.munaiah@live.com>
2015-12-06 19:55:29 -0500

    Refactored subject classes to improve readability
    Refactors:
    
     * Removed hardcoded clone_url and remote_url. These are now in
       app.constants.
     * Reflected  member renames from parent class.

--- app/subjects/curl.py
+++ app/subjects/curl.py
@@ -5,63 +5,43 @@ from app.subjects import subject
 
 
 class cURL(subject.Subject):
-    def __init__(
-            self, configure_options, processes=1, git_reference=None,
-            scratch_root='~'
-    ):
+    def __init__(self, name, remote, scratch_root):
+        super().__init__(name, remote, scratch_root)
 
-        name = 'cURL'
-        clone_url = 'https://github.com/bagder/curl.git'
-        remote_url = (
-            'https://5751f28e22fbde2d8ba3f9b75936e0767c761c6a.'
-            'googledrive.com/host/0B1eWsh8KZjRrfjg0Z1VkcU96U2h'
-            'Qal9scHM3NzVmYTk2WVhiQzQtdGFpNWc5c0VzbUJFTE0/cURL/'
-        )
-
-        if '~' in scratch_root:
-            scratch_root = os.path.expanduser(scratch_root)
-
-        super().__init__(
-            name, clone_url, configure_options, processes, git_reference,
-            remote_url, scratch_root
-        )
-
-    def configure(self):
-        self.__dbug__('Configuring {0}'.format(self.name))
+    def configure(self, options):
+        self.debug('Configuring {0}'.format(self.name))
 
         returncode = self.execute('./buildconf')
         if returncode == 0:
-            returncode = self.execute('./configure {0}'.format(
-                self.configure_options
-            ))
+            returncode = self.execute('./configure {0}'.format(options))
         return returncode
 
-    def make(self):
-        self.__dbug__('Building {0}'.format(self.name))
-        cmd = 'make -j %d' % self.processes
+    def make(self, processes=1):
+        self.debug('Building {0}'.format(self.name))
+        cmd = 'make -j %d' % processes
         return self.execute(cmd)
 
-    def test(self):
-        self.__dbug__('Testing {0}'.format(self.name))
-        cmd = 'make -j %d -C tests' % self.processes
+    def test(self, processes=1):
+        self.debug('Testing {0}'.format(self.name))
+        cmd = 'make -j %d -C tests' % processes
         self.execute(cmd)
 
         # Returning non-zero return value to allow execution of manual script
         return 2
 
-    def __gprof__(self, gmon_file_path, gprof_file_path):
-        self.__dbug__(
+    def __gprof__(self, gmon_path, gprof_path):
+        self.debug(
             'Generating call graph for {0} using gprof with profile '
-            'information from {1}'.format(self.name, gmon_file_path)
+            'information from {1}'.format(self.name, gmon_path)
         )
-        if 'basegmon.out' in gmon_file_path:
+        if 'basegmon.out' in gmon_path:
             cmd = 'gprof -q -b -l -c -z -L src/.libs/curl {0}'
         else:
             cmd = 'gprof -q -b -l -c -L src/.libs/curl {0}'
 
-        cmd = cmd.format(gmon_file_path, gprof_file_path)
+        cmd = cmd.format(gmon_path, gprof_path)
 
-        with open(gprof_file_path, 'w+') as _gprof_file:
+        with open(gprof_path, 'w+') as _gprof_file:
             returncode = self.execute(cmd, stdout=_gprof_file)
 
         # gprof's -L is printing absolute path instead of relative path.
@@ -73,20 +53,17 @@ class cURL(subject.Subject):
 
         self.execute(
             "sed -i 's;{0}\/src\/\.\.;.;g' {1}".format(
-                self.source_dir.replace('/', '\/'),
-                gprof_file_path
+                self.source_dir.replace('/', '\/'), gprof_path
             )
         )
         self.execute(
             "sed -i 's;{0}\/src;.;g' {1}".format(
-                self.source_dir.replace('/', '\/'),
-                gprof_file_path
+                self.source_dir.replace('/', '\/'), gprof_path
             )
         )
         self.execute(
             "sed -i 's;{0};.;g' {1}".format(
-                self.source_dir.replace('/', '\/'),
-                gprof_file_path
+                self.source_dir.replace('/', '\/'), gprof_path
             )
         )
 
--- app/subjects/ffmpeg.py
+++ app/subjects/ffmpeg.py
@@ -5,54 +5,36 @@ from app.subjects import subject
 
 
 class FFmpeg(subject.Subject):
-    def __init__(
-            self, configure_options, processes=1, git_reference=None,
-            scratch_root='~'
-    ):
+    def __init__(self, name, remote, scratch_root):
+        super().__init__(name, remote, scratch_root)
 
-        name = 'FFmpeg'
-        clone_url = 'https://github.com/FFmpeg/FFmpeg.git'
-        remote_url = (
-            'https://5751f28e22fbde2d8ba3f9b75936e0767c761c6a.'
-            'googledrive.com/host/0B1eWsh8KZjRrfjg0Z1VkcU96U2h'
-            'Qal9scHM3NzVmYTk2WVhiQzQtdGFpNWc5c0VzbUJFTE0/FFmpeg/'
-        )
-
-        if '~' in scratch_root:
-            scratch_root = os.path.expanduser(scratch_root)
-
-        super().__init__(
-            name, clone_url, configure_options, processes, git_reference,
-            remote_url, scratch_root
-        )
-
-    def configure(self):
-        self.__dbug__('Configuring {0}'.format(self.name))
-        cmd = './configure {0}'.format(self.configure_options)
+    def configure(self, options):
+        self.debug('Configuring {0}'.format(self.name))
+        cmd = './configure {0}'.format(options)
         return self.execute(cmd)
 
-    def make(self):
-        self.__dbug__('Building {0}'.format(self.name))
-        cmd = 'make -j %d' % self.processes
+    def make(self, processes=1):
+        self.debug('Building {0}'.format(self.name))
+        cmd = 'make -j %d' % processes
         return self.execute(cmd)
 
-    def test(self):
-        self.__dbug__('Testing {0}'.format(self.name))
-        cmd = 'make -j %d fate-rsync' % self.processes
+    def test(self, processes):
+        self.debug('Testing {0}'.format(self.name))
+        cmd = 'make -j %d fate-rsync' % processes
         self.execute(cmd)
 
         # Returning non-zero return value to allow execution of manual script
         return 2
 
-    def __gprof__(self, gmon_file_path, gprof_file_path):
-        self.__dbug__(
+    def __gprof__(self, gmon_path, gprof_path):
+        self.debug(
             'Generating call graph for {0} using gprof with profile '
-            'information from {1}'.format(self.name, gmon_file_path)
+            'information from {1}'.format(self.name, gmon_path)
         )
         cmd = 'gprof -q -b -l -c -L ffmpeg_g {0}'
-        cmd = cmd.format(gmon_file_path)
+        cmd = cmd.format(gmon_path)
 
-        with open(gprof_file_path, 'w+') as _gprof_file:
+        with open(gprof_path, 'w+') as _gprof_file:
             returncode = self.execute(cmd, stdout=_gprof_file)
 
         # gprof's -L is printing absolute path instead of relative path.
@@ -64,20 +46,17 @@ class FFmpeg(subject.Subject):
 
         self.execute(
             "sed -i 's;{0}\/\.;.;g' {1}".format(
-                self.source_dir.replace('/', '\/'),
-                gprof_file_path
+                self.source_dir.replace('/', '\/'), gprof_path
             )
         )
         self.execute(
             "sed -i 's;{0}\/\/;./;g' {1}".format(
-                self.source_dir.replace('/', '\/'),
-                gprof_file_path
+                self.source_dir.replace('/', '\/'), gprof_path
             )
         )
         self.execute(
             "sed -i 's;{0};.;g' {1}".format(
-                self.source_dir.replace('/', '\/'),
-                gprof_file_path
+                self.source_dir.replace('/', '\/'), gprof_path
             )
         )
 
