fef9cf3552ee80e3250bf2824b31164210b54838
Nuthan Munaiah <nuthan.munaiah@live.com>
2016-01-05 02:09:56 -0500

    Added helper to generate paramaters
    The generate_parameters method generates the parameters used when
    analyzing the sensitivity of Page Rank to varying parameters.

--- app/helpers.py
+++ app/helpers.py
@@ -1,5 +1,8 @@
+import csv
 import os
 import sys
+
+import numpy
 
 from django.conf import settings
 
@@ -26,6 +29,31 @@ def get_version_components(string):
 
 def get_absolute_path(dir_name):
     return os.path.join(settings.BASE_DIR, dir_name)
+
+
+def generate_parameters(filepath):
+    parameters_collection = list()
+
+    other = 1   # Personalization value for all non-entry/exit functions
+
+    # Damping factor from 10% to 90% with 5% increments
+    for damping in numpy.arange(0.1, 1.0, 0.05):
+        # Personalization from 1 to 1000000 increasing exponentially
+        for power in range(0, 7):
+            entry = 10 ** power
+            for power in range(0, 7):
+                exit = 10 ** power
+                for power in range(0, 5):
+                    call = 10 ** power
+                    for power in range(0, 5):
+                        retrn = 10 ** power
+                        parameters_collection.append((
+                            round(damping, 2), entry, exit, other, call, retrn
+                        ))
+
+    with open(filepath, 'w') as file_:
+        writer = csv.writer(file_)
+        writer.writerows(parameters_collection)
 
 
 def debug(message, line=False):
